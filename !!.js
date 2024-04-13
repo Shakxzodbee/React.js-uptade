defe6c1c70da6a08e582edb02f498ce70c00c527
Shahzodbek <136874725+Shakxzodbee@users.noreply.github.com>
2024-04-13 18:59:08 +0500

    Update !!.js

--- !!.js
+++ !!.js
@@ -8,3 +8,9 @@ let = 'Node.js terminal verisyasini korsatib beradi'
 
 
 // {`pwd`} qaysi manzilda turganligimizni aniqlab beradi
+
+
+ReactDOM.render(
+  1-element html bo'lishi kerak 
+);
+React.strikt mod ni vazifasi yangilanishdagi o'zgarishlarni olib tashlaydi 
